1f3abb1629a6176bf710dc077a27ae7785862b9f
pinguyz-lgtm <pinguyweb@gmail.com>
2026-06-30 14:18:19 +0200

    Fixes
    fix: navbar trasparente, timeout GPS, nuova icona MyBenza, versionCode 4

--- android/app/src/main/java/it/pinguyweb/benza/MainActivity.java
+++ android/app/src/main/java/it/pinguyweb/benza/MainActivity.java
@@ -1,5 +1,12 @@
 package it.pinguyweb.benza;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setDecorFitsSystemWindows(false);
+    }
+}
